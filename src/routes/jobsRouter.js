d9c9945fa61f190ce1bd2a12e394775e963aab9e
Kaipo <kaipojames12@gmail.com>
2023-04-04 19:54:14 -0500

    add comments

--- src/routes/jobsRouter.js
+++ src/routes/jobsRouter.js
@@ -5,11 +5,13 @@ import { validateNewAvailableJob } from '../utils/validation/ValidatorUtil.js';
 
 const jobs = express.Router();
 
+// Available Jobs
 jobs.get('/', async (req, res) => await getAvailableJobs(req, res));
 jobs.post('/', validateNewAvailableJob(), async (req, res) => await createAvailableJob(req, res));
 jobs.get('/:id', async (req, res) => await getAvailableJob(req, res));
 jobs.patch('/:id', async (req, res) => await updateAvailableJob(req, res));
 
+// Cron Jobs
 jobs.get('/active', async (req, res) => await getActiveJobs(req, res));
 jobs.post('/:jobName', async (req, res) => await createJob(req, res));
 jobs.get('/:jobName/trigger', async (req, res) => await triggerJob(req, res)); // When you trigger, the job should already be started
